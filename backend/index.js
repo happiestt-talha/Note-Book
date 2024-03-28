d68b7674824117d15f01ccb0042d409ad711d065
Talha <mtalhamanzoor786@gmail.com>
2024-03-28 10:04:34 -0700

    adding db now

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,13 @@
 const connectToMongo=require('./db')
+const express = require('express')
 connectToMongo()
+const app = express()
+const port = 3000
+
+app.use(express.json())
+// 
+app.get('/api/auth',require('./routes/auth'))
+// app.get('/api/notes',require('./routes/notes'))
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
