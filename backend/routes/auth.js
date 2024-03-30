5bcd0e5669620e14b0a5e9fe8e86ed83ac41bc3c
Talha <mtalhamanzoor786@gmail.com>
2024-03-30 02:47:25 -0700

    Password validation done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,20 @@
 const express=require('express')
+const { body, validationResult } = require('express-validator');
+
 const router =express.Router();
 const User=require('../models/User')
-router.get('/',(req,res)=>{
+
+router.post('/',[
+    body('email').isEmail().withMessage('Not a valid email bro'),
+    body('password').isLength({ min: 5 }).withMessage('Itna Chota Password!!')
+],(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
     console.log(req.body);
-    // res.json(obj)
     const user=User(req.body);
     user.save();
     res.send('Hello from Auth Body');
-    // res.send(req.body)
 })
 module.exports=router 
