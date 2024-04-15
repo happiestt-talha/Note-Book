d46baa542c07574ac5c788f44a7513e7fe2ce802
Talha <mtalhamanzoor786@gmail.com>
2024-04-15 19:20:39 +0500

    done with all errors, Alhamdulillah... as far i think there was an issue with collection, otherwise everything was working fine at both sides

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose')
 const noteSchema=new mongoose.Schema({
-    // user:{
-    //     type:mongoose.Schema.Types.ObjectId,
-    //     ref:'User'
-    // },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
     title:{
         type:String,
         required:true
@@ -21,4 +21,4 @@ const noteSchema=new mongoose.Schema({
         default:Date.now
     }
 })
-module.exports=mongoose.model('Note',noteSchema,'notes')
+module.exports=mongoose.model('Note',noteSchema,'usernotes')
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -5,34 +5,38 @@ const fetchUser = require('../middlewares/fetchUser');
 const Note = require('../models/Note');
 
 // GET /api/notes
-router.get('/', fetchUser, async (req, res) => {
+router.get('/getnotes', fetchUser, async (req, res) => {
     try {
-        const userNotes = await Note.find({ user: req.user._id });
-        res.json(userNotes);
+        // res.json({reqUser:req.user})
+        const userNotes = await Note.find({ user: req.user.id });
+        res.json({reqUser:req.user,userNotes});
+        // res.json(userNotes);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
     }
 });
 
+
 // POST /api/notes
-router.post('/', fetchUser, [
+router.post('/addnote', fetchUser, [
     body('title').notEmpty().withMessage('Title is required'),
     body('description').notEmpty().withMessage('Description is required')
 ], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-
+    // res.json({reqUser:req.user})
     const { title, description, tag } = req.body;
     try {
         const newNote = new Note({
-            user: req.user._id,
+            user: req.user.id,
             title,
             description,
             tag
         });
         const savedNote = await newNote.save();
-        res.json(savedNote);
+        res.json({reqUser:req.user,savedNote});
+        // res.json(savedNote);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
