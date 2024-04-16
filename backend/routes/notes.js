e6e1da6e0960745d01feb27b337a31f1cc651606
Talha <mtalhamanzoor786@gmail.com>
2024-04-16 20:38:32 +0500

    Note deletion module added, Usef able to delete any node , any time..... Backend Almost completed for now, will now work on FE side

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -9,7 +9,7 @@ router.get('/getnotes', fetchUser, async (req, res) => {
     try {
         // res.json({reqUser:req.user})
         const userNotes = await Note.find({ user: req.user.id });
-        res.json({reqUser:req.user,userNotes});
+        res.json({ reqUser: req.user, userNotes });
         // res.json(userNotes);
     } catch (err) {
         console.error(err.message);
@@ -35,7 +35,7 @@ router.post('/addnote', fetchUser, [
             tag
         });
         const savedNote = await newNote.save();
-        res.json({reqUser:req.user,savedNote});
+        res.json({ reqUser: req.user, savedNote });
         // res.json(savedNote);
     } catch (err) {
         console.error(err.message);
@@ -47,11 +47,11 @@ router.post('/addnote', fetchUser, [
 router.put('/updatenote/:id', fetchUser, async (req, res) => {
     try {
         const { title, description, tag } = req.body;
-        const newNote={}
-        
-        if(title){newNote.title=title}
-        if(description){newNote.description=description}
-        if(tag){newNote.tag=tag}
+        const newNote = {}
+
+        if (title) { newNote.title = title }
+        if (description) { newNote.description = description }
+        if (tag) { newNote.tag = tag }
 
         let note = await Note.findById(req.params.id);
         if (!note) {
@@ -63,11 +63,34 @@ router.put('/updatenote/:id', fetchUser, async (req, res) => {
         }
 
         note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
-        res.json({note});
+        res.json({ note });
     }
     catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
     }
+})
+
+
+router.delete('/deletenote/:id', fetchUser, async (req, res) => {
+
+    try {
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send('Not Found');
+        }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send('Not Allowed');
+        }
+        note =await Note.findByIdAndDelete(req.params.id)
+        res.json({ "Success": "Note has been deleted", note: note });
+    }
+
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
 })
 module.exports = router;
