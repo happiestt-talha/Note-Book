1e519cd2cbed044c3ef9572b988f264a6f9ab7a7
Talha <mtalhamanzoor786@gmail.com>
2024-04-19 21:42:53 +0500

    added useLocation feature to Make Navbar more UI friendly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,8 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Link,useLocation } from 'react-router-dom'
 
 export const Navbar = () => {
   const location=useLocation();
-  useEffect(() => {
-    console.log(location.pathname);
-  }, [location])
   
   return (
     <>
